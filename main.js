e0e2366554022143f18dc6ab4caa3ae18a143abb
RyanD177 <w0477770@campus.nscc.ca>
2023-09-05 03:46:42 -0300

    z

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 /*
+Goal is to fill every cell of a 9x9 grid = 81 squares, with the numbers 1-9 such that no dupilicates appear in any row, column, or 
+3x3 subgrid.
+
+ 2 types of cells given nums and standard cells.
+
+ given nums = class .given-number
+ should have no func
+
+ standard cells = 2 child spans. Classes .candidates and .value 
+ when clicked, it should become selected (the cell should have the selected class) Only one cell should be selectable at a time (should unselect the prev)
+
+
+ controls
+
+ #candidate-switch is unchecked(set to normal move) clicking on any num should place the num in the .value span of selected cell.
+
 
 */
+
